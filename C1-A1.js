600fc93b4ff07ff46e4905c283b79a948a2f3715
mitzie-ponce <mitzieponce0@gmail.com>
2025-04-06 23:39:24 +0800

    Create C1-A1.js

---
+++ C1-A1.js
@@ -0,0 +1,47 @@
+let firstName1 = "Avril";
+let middleName1 = "Sabalo";
+let lastName1 = "Galimba";
+let birthDate1 = "April 15, 2006";
+let birthPlace1 = "Langlangca 2nd,Candon City,Ilocos Sur, Philippines 2710";
+let address1 = "Langlangca 2nd,Candon City,Ilocos Sur,Philippines 2710";
+let courseYear1 = "Bachelor of Science in Computer Science";
+let dreamJob1 = "Web Developer";
+
+let studentInfo1 = '$
+(firstName1.toUpperCase ()} $
+{middleName1.toUpperCase()} $
+{lastName1.toUpperCase()} was born on
+[${birthDate1}] at [${birthPlace1}],
+and currently living at [${address1}] 
+${firstName1.toLowerCase()} $
+{middleName1.toLowerCase()} $
+{lastName1.toLowerCase()} is taking up
+[${courseYear1}] and dreams to be [$
+{dreamJob1}] after graduation.';
+
+console.log(studentInfo1):
+    
+let firstName2 = "Mitzie Jamelle";
+let middleName2 = "Abuan";
+let lastName2 = "Ponce";
+let birthDate2 = "August 05, 2006";
+let birthPlace2 = "Bul-bulala,Santiago,Ilocos Sur, Philippines 2710";
+let address2 = "Bul-bulala,Santiago,Ilocos Sur,Philippines 2710";
+let courseYear2 = "Bachelor of Science in Computer Science";
+let dreamJob2 = "Software Engineer";
+
+let studentInfo2 = '$
+(firstName2.toUpperCase ()} $
+{middleName2.toUpperCase()} $
+{lastName2.toUpperCase()} was born on
+[${birthDate2}] at [${birthPlace2}],
+and currently living at [${address2}] 
+${firstName2.toLowerCase()} $
+{middleName2.toLowerCase()} $
+{lastName2.toLowerCase()} is taking up
+[${courseYear2}] and dreams to be [$
+{dreamJob2}] after graduation.';
+
+console.log(studentInfo2):
+    
+    
