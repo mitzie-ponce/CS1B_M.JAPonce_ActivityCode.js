bae36be8ef68d344a39a916b565b6964eb597344
mitzie-ponce <mitzieponce0@gmail.com>
2025-04-06 23:53:48 +0800

    Create C1-A2.js

---
+++ C1-A2.js
@@ -0,0 +1,24 @@
+let a = 25;
+let b = 20;
+let c = 12;
+const e = 15;
+
+let sum = a + b + c + e;
+console. log('The sum of a, b, c, and e is: ${sum}');
+
+let d = 15;
+console.log('Is d < e? ${d < e}');
+console.log('Is d > e? ${d > e}');
+console.log('Is d === e? ${d === e}');
+
+let subtraction Result = a - b -c - d;
+console.log(`Result of subtracting a, b, c, and d: ${subtractionResult});
+
+let multiplicationDivisionResult = (a * c) / e;
+console.log(`Result of multiplying a and c, divided by e: ${multiplicationDivis ion Result});
+
+let exponentResult = Math.pow(e, c);
+console.log(`Result of e raised to the power of c: ${exponentResult}');
+
+const newE = Math.pow(c, 3);
+console.log('New value of e (c^3): ${newE}');
